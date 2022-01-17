56e813d895b5e90baad248c2daa6d4ebd3f05984
rrajpald <rajpal.diwaker@ingka.com>
2022-01-17 20:00:55 +0530

    16 time

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -18,6 +18,11 @@ output "self_link" {
   description = "The URI of the External Static IP resource."
 }
 
+output "email" {
+  value       = google_service_account.gce_sa.email
+  description = "The URI of the Google Service Account resource."
+}
+
 output "sa_id" {
   value       = local.sa_id
   description = "Display Name of created Google Service Account"
