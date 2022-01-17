e2b23c173efda0ea7ac97bffc59c73341edfad65
rrajpald <rajpal.diwaker@ingka.com>
2022-01-17 19:28:05 +0530

    13 time

--- examples/main.tf
+++ examples/main.tf
@@ -8,23 +8,23 @@ locals {
   suffix = format("%s-%s", "tf", '12')
 }
 
-#module "kylo_ren" {
-#  source           = "../modules/gce"
-#  suffix           = local.suffix
-#  gcp_project_id   = var.gcp_project_id
-#  vpc_network_name = "default"
-#  instance_name    = "kylo-ren"
-#  network_tags     = ["http-server", "https-server"]
-#}
+module "kylo_ren" {
+  source           = "../modules/gce"
+  suffix           = local.suffix
+  gcp_project_id   = var.gcp_project_id
+  vpc_network_name = "default"
+  instance_name    = "kylo-ren"
+  network_tags     = ["http-server", "https-server"]
+}
 
 
 ######## for New VPC module create
 
-module "vpc" {
-  source           = "../modules/vpc"
-  suffix           = local.suffix
-  gcp_project_id   = var.gcp_project_id
-  vpc_network_name = "default"
-  instance_name    = "palpatine"
-  network_tags     = ["http-server", "https-server"]
-}
+#module "vpc" {
+#  source           = "../modules/vpc"
+#  suffix           = local.suffix
+#  gcp_project_id   = var.gcp_project_id
+#  vpc_network_name = "default"
+#  instance_name    = "palpatine"
+#  network_tags     = ["http-server", "https-server"]
+#}
