3bbc01beaaff51c303b909c0572071e8e46c86bb
rrajpald <rajpal.diwaker@ingka.com>
2022-01-18 12:59:31 +0530

    vpc changes 13

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,31 @@
-module "vpc" {
+resource "random_string" "launch_id" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+locals {
+  suffix = format("%s-%s", "tf", "2")
+}
+
+#module "kylo_ren" {
+#  source           = "../modules/gce"
+#  suffix           = local.suffix
+#  gcp_project_id   = var.gcp_project_id
+#  vpc_network_name = "default"
+#  instance_name    = "kylo-ren"
+#  network_tags     = ["http-server", "https-server"]
+#}
+
+#module "custom-vpc" {
+#  source           = "../modules/vpc2"
+#  gcp_project_id   = "custom-valve-332208"
+#  vpc_network_name = "custom-vpc2"
+#}
+
+module "management_network" {
   source = "../modules/vpc"
+#  name = "devTest"
+#  project     = "custom-valve-332208"
+#  region      = "us-east1"
 }
