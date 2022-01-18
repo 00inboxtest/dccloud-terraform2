5ed7a1178def1aece2d115b01aa004884fea1cd2
rrajpald <rajpal.diwaker@ingka.com>
2022-01-18 12:15:15 +0530

    vpc changes 5

--- examples/main.tf
+++ examples/main.tf
@@ -24,10 +24,7 @@ locals {
 #}
 
 module "management_network" {
-  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
-  # to a specific version of the modules, such as the following example:
-  # source = "github.com/gruntwork-io/terraform-google-network.git//modules/vpc-network?ref=v0.1.2"
-  
+    
   source = "../modules/vpc2"
   name_prefix = "devTest"
   project     = "custom-valve-332208"
