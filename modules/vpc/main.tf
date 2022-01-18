5724b8c8b12f6f06925da0183c5c6bec7d18971c
rrajpald <rajpal.diwaker@ingka.com>
2022-01-18 13:49:02 +0530

    vpc changes 14

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,14 @@
-resource "google_compute_network" "vpc_network" {
-  project                 = "custom-valve-332208"
-  name                    = "vpc-networks"
-  auto_create_subnetworks = true
-  mtu                     = 1460
+resource "google_compute_subnetwork" "network-for-l7lb" {
+  provider = google-beta
+  name          = "l7lb-test-subnetwork"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "us-central1"
+  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
+  role          = "ACTIVE"
+  network       = google_compute_network.custom-test.id
+}
+resource "google_compute_network" "custom-test" {
+  provider = google-beta
+  name                    = "l7lb-test-network"
+  auto_create_subnetworks = false
 }
