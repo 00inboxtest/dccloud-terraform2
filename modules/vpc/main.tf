c7763c5969feea71af6279e6fe5710e6275591e3
rrajpald <rajpal.diwaker@ingka.com>
2022-01-18 13:51:51 +0530

    vpc changes 14

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,14 +1,20 @@
-resource "google_compute_subnetwork" "network-for-l7lb" {
-  provider = google-beta
-  name          = "l7lb-test-subnetwork"
-  ip_cidr_range = "10.0.0.0/24"
-  region        = "us-central1"
-  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
-  role          = "ACTIVE"
-  network       = google_compute_network.custom-test.id
-}
-resource "google_compute_network" "custom-test" {
-  provider = google-beta
-  name                    = "l7lb-test-network"
-  auto_create_subnetworks = false
+resource "google_compute_network" "network" {
+    name        = "custom-vpc"
+    project     = "${var.project_id}"
+    subnetworks = [
+        {
+            region           = "us-east1"
+            cidr             = "192.168.11.0/24"
+            enable_flow_logs = "true"
+            private_ip_google_access = "false"
+        },
+        {
+            region  = "us-east4"
+            cidr    = "192.168.12.0/24"
+        }
+    ]
+
+    // pass this when creating custom subnetworks
+    auto_create_subnetworks = "false"
+
 }
--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,34 +1,14 @@
-output "network_name" {
-  value       = local.network_name
-  description = "Name of GCE VM Instance."
+output "vpc_name" {
+  value       = "${module.custom-vpc.name}"
+  description = "Name of the created VPC."
 }
 
-output "id" {
-  value       = google_compute_instance.gce.id
-  description = "An identifier for the resource with format projects/{{project}}/zones/{{zone}}/instances/{{name}}"
-}
-
-output "instance_id" {
-  value       = google_compute_instance.gce.instance_id
-  description = "The server-assigned unique identifier of this instance. Example: 4567719474035761998"
+output "vpc_gateway" {
+  value       = "${module.custom-vpc.gateway_ipv4}"
+  description = "The IPv4 address of the gateway from the created VPC."
 }
 
 output "self_link" {
-  value       = google_compute_address.gce_static_ip.self_link
-  description = "The URI of the External Static IP resource."
-}
-
-output "email" {
-  value       = google_service_account.gce_sa.email
-  description = "The URI of the Google Service Account resource."
-}
-
-output "sa_id" {
-  value       = local.sa_id
-  description = "Display Name of created Google Service Account"
-}
-
-output "region" {
-  value       = data.google_client_config.google_client.region
-  description = "GCP Region in which GCE VM Instance being created"
+  value       = "${module.custom-vpc.self_link}"
+  description =  "The URI of the created VPC."
 }
