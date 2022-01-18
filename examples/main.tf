1eb3f1cd4410e0d146686592757088f8042023fe
rrajpald <rajpal.diwaker@ingka.com>
2022-01-18 10:09:23 +0530

    latest chnages

--- examples/main.tf
+++ examples/main.tf
@@ -6,23 +6,14 @@ resource "random_string" "launch_id" {
 
 locals {
   suffix = format("%s-%s", "tf", random_string.launch_id.result)
-  #suffix = format("%s-%s", "tf", '12')
 }
-
-#module "kylo_ren" {
-#  source           = "../modules/gce"
-#  suffix           = local.suffix
-#  gcp_project_id   = var.gcp_project_id
-#  vpc_network_name = "default"
-#  instance_name    = "kylo-ren"
-#  network_tags     = ["http-server", "https-server"]
-#}
 
 module "kylo_ren" {
   source           = "../modules/gce"
   suffix           = local.suffix
   gcp_project_id   = var.gcp_project_id
   vpc_network_name = "default"
-  instance_name    = "dev-test"
+  instance_name    = "kylo-ren"
   network_tags     = ["http-server", "https-server"]
 }
+
