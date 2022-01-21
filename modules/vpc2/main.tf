a692d24bdfc56d897fd40d12fe96ae66ddb0d982
rrajpald <rajpal.diwaker@ingka.com>
2022-01-21 11:46:48 +0530

    vpc module correction and deployement

--- modules/vpc2/main.tf
+++ modules/vpc2/main.tf
@@ -1,26 +1,32 @@
+locals {
+  name             = "terraform-networks2"
+  region           = "europe-west1"
+  pub-subnet-cidr    = 10.2.0.0/16
+}
+
 ## Create VPC
 resource "google_compute_network" "vpc" {
-     name                    = "${var.name}-vpc"
+     name                    = "${local.name}-vpc"
      auto_create_subnetworks = "false"
     }
     ## Create Subnet
  ##  Create public subnet  
 
 resource "google_compute_subnetwork" "pub-subnet" {
-     name          = "${var.name}-public-subnet"
-     ip_cidr_range = "${var.pub-subnet-cidr}"
-     network       = "${var.name}-vpc"
+     name          = "${local.name}-public-subnet"
+     ip_cidr_range = "${local.pub-subnet-cidr}"
+     network       = "${local.name}-vpc"
      depends_on    = ["google_compute_network.vpc"]
-     region        = "us-east1"
+     region        = ${local.region}
     }
 
 ## Create private subnet
 
 resource "google_compute_subnetwork" "private-subnet" {
-     name          = "${var.name}-private-subnet"
-     ip_cidr_range = "${var.private-subnet-cidr}"
-     network       = "${var.name}-vpc"
+     name          = "${local.name}-private-subnet"
+     ip_cidr_range = "${local.private-subnet-cidr}"
+     network       = "${local.name}-vpc"
      depends_on    = ["google_compute_network.vpc"]
-     region        = "us-east1"
+     region        = ${local.region}
      private_ip_google_access = "true"
     }
