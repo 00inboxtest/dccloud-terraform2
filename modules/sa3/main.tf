2a401d50870f2f8aa519e3d51bf3e6a6c44a8709
rrajpald <rajpal.diwaker@ingka.com>
2022-01-28 19:02:56 +0530

    code push gce

---
+++ modules/sa3/main.tf
@@ -0,0 +1,21 @@
+
+locals {
+  project_id = "custom-valve-332208"
+}
+
+############################
+# SERVICE ACCOUNT (OPTIONAL)
+############################
+# Note: The user running terraform needs to have the IAM Admin role assigned to them before you can do this.
+
+resource "google_service_account" "instance_admin" { 
+  account_id   = "instance-admin"
+  display_name = "instance s-account"
+  }
+
+ resource "google_project_iam_binding" "instance_sa_iam" {
+  project = local.project_id
+  role    = "roles/compute.instanceAdmin.v1"
+  members = [
+    "serviceAccount:${google_service_account.instance_admin.email}"
+  ]
--- setup/main.tf
+++ setup/main.tf
@@ -2,14 +2,14 @@ locals {
   suffix = format("%s-%s", "tf", "2")
 }
 
-module "gce" {
-  source           = "../modules/gce"
-  suffix           = local.suffix
-  gcp_project_id   = var.gcp_project_id
-  vpc_network_name = "default"
-  instance_name    = "kylo-ren"
-  network_tags     = ["http-server", "https-server"]
-}
+#module "gce" {
+#  source           = "../modules/gce"
+#  suffix           = local.suffix
+#  gcp_project_id   = var.gcp_project_id
+#  vpc_network_name = "default"
+#  instance_name    = "kylo-ren"
+#  network_tags     = ["http-server", "https-server"]
+#}
 
 #module "iam" {
 #  source           = "../modules/iam"
@@ -29,3 +29,7 @@ module "gce" {
 #  suffix           = local.suffix
 #  gcp_project_id   = var.gcp_project_id
 #}
+
+module "sa" {
+  source = "../modules/sa3"
+}
