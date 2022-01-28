a97406c124b8d1e06a87f7e34b86dcbeba9c507b
rrajpald <rajpal.diwaker@ingka.com>
2022-01-28 18:36:08 +0530

    code push

--- modules/sa/main.tf
+++ modules/sa/main.tf
@@ -2,11 +2,11 @@ locals {
   project_id = "custom-valve-332208"
 }
 
-#resource "google_project_service" "project" {
-#  project = local.project_id
-#  service = "iam.googleapis.com"
-#  disable_dependent_services = true
-#}
+resource "google_project_service" "project" {
+  project = local.project_id
+  service = "iam.googleapis.com"
+  disable_dependent_services = true
+}
 
 #resource "google_service_account" "sa-name" {
 #  account_id = "devtest"
