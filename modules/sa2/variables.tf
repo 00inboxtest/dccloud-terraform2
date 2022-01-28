535af3cc3ccc92e910d1c7d33b86a482aa55dacc
rrajpald <rajpal.diwaker@ingka.com>
2022-01-28 18:51:21 +0530

    code push

--- modules/sa2/variables.tf
+++ modules/sa2/variables.tf
@@ -12,3 +12,8 @@ variable "sa_timeout" {
   default     = "10m"
   description = "How long a Service Account creation operation is allowed to take before being considered a failure."
 }
+
+variable "gcp_project_id" {
+  type        = string
+  description = "Project in which GCE Resources will be created."
+}
