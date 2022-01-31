5c18b8d605432f53463d21ad5faeefe1701c977c
rrajpald <rajpal.diwaker@ingka.com>
2022-01-31 12:40:59 +0530

    code push sa 3

--- modules/sa3/main.tf
+++ modules/sa3/main.tf
@@ -8,12 +8,9 @@ locals {
 ############################
 # Note: The user running terraform needs to have the IAM Admin role assigned to them before you can do this.
 
-resource "google_project_iam_custom_role" "my-instance-role" {
-  role_id     = "myInstanceRole"
-  title       = "My Instance Role"
-  description = "my custom iam role"
-  permissions = [
-    "storage.objects.create", 
-    "cloudkms.cryptoKeyVersions.useToEncrypt"
-  ]
+resource "google_project_iam_custom_role" "my-custom-role" {
+  role_id     = "myCustomRole"
+  title       = "My Custom Role"
+  description = "A description"
+  permissions = ["iam.roles.list", "iam.roles.create", "iam.roles.delete"]
 }
