1f3a46481d911274996d0b8cdeb6da211bb8a67c
rrajpald <rajpal.diwaker@ingka.com>
2022-02-01 11:44:33 +0530

    code depoy to prod

--- setup/main.tf
+++ setup/main.tf
@@ -17,14 +17,14 @@ module "custom-vpc" {
 }
 
 ######## compute instance creation #####
-module "gce" {
-  source           = "../modules/gce"
-  suffix           = local.suffix
-  gcp_project_id   = var.gcp_project_id
-  vpc_network_name = "terraform-networks"
-  instance_name    = "test"
-  network_tags     = ["http-server", "https-server"]
-}
+#module "gce" {
+#  source           = "../modules/gce"
+#  suffix           = local.suffix
+#  gcp_project_id   = var.gcp_project_id
+#  vpc_network_name = "terraform-networks"
+#  instance_name    = "test"
+#  network_tags     = ["http-server", "https-server"]
+#}
 
 ######## for New service account creation #####
 module "service_account" {
@@ -34,7 +34,6 @@ module "service_account" {
   gcp_project_id   = var.gcp_project_id
 }
 
-
 #module "iam" {
 #  source           = "../modules/iam"
 #}
@@ -43,4 +42,46 @@ module "service_account" {
 #  source           = "../modules/vpc2"
 #}
 
+###### vpc correction #######
+module "vpc" {
+    source  = "terraform-google-modules/network/google//modules/subnets"
+    version = "~> 2.0.0"
+    project_id   = "custom-valve-332208"
+    network_name = "example-vpc"
 
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-west1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-west1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        },
+        {
+            subnet_name               = "subnet-03"
+            subnet_ip                 = "10.10.30.0/24"
+            subnet_region             = "us-west1"
+            subnet_flow_logs          = "true"
+            subnet_flow_logs_interval = "INTERVAL_10_MIN"
+            subnet_flow_logs_sampling = 0.7
+            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+        }
+    ]
+
+    secondary_ranges = {
+        subnet-01 = [
+            {
+                range_name    = "subnet-01-secondary-01"
+                ip_cidr_range = "192.168.64.0/24"
+            },
+        ]
+
+        subnet-02 = []
+    }
+}
