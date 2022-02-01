73502a8fca5d808f7ae8bc19a16f9a0ba39afabb
rrajpald <rajpal.diwaker@ingka.com>
2022-02-01 11:22:28 +0530

    code depoy to prod

--- setup/main.tf
+++ setup/main.tf
@@ -7,14 +7,14 @@ module "service_enable" {
   gcp_project_id   = var.gcp_project_id
 }
 
-#module "gce" {
-#  source           = "../modules/gce"
-#  suffix           = local.suffix
-#  gcp_project_id   = var.gcp_project_id
-#  vpc_network_name = "default"
-#  instance_name    = "kylo-ren"
-#  network_tags     = ["http-server", "https-server"]
-#}
+module "gce" {
+  source           = "../modules/gce"
+  suffix           = local.suffix
+  gcp_project_id   = var.gcp_project_id
+  vpc_network_name = "default"
+  instance_name    = "kylo-ren"
+  network_tags     = ["http-server", "https-server"]
+}
 
 #module "iam" {
 #  source           = "../modules/iam"
@@ -32,9 +32,6 @@ module "custom-vpc" {
   ip_cidr_range    = "10.2.0.0/16"
 }
 
-#module "sa" {
-#  source = "../modules/sa"
-#}
 
 # for New service account
 #module "service_account" {
