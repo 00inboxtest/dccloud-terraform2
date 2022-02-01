159385ce79e25ddfab22b81a7bc6ed0565f584f4
rrajpald <rajpal.diwaker@ingka.com>
2022-02-01 11:25:12 +0530

    code depoy to prod

--- setup/main.tf
+++ setup/main.tf
@@ -11,8 +11,8 @@ module "gce" {
   source           = "../modules/gce"
   suffix           = local.suffix
   gcp_project_id   = var.gcp_project_id
-  vpc_network_name = "default"
-  instance_name    = "kylo-ren"
+  vpc_network_name = "terraform-networks"
+  instance_name    = "test"
   network_tags     = ["http-server", "https-server"]
 }
 
